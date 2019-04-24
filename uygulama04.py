2c7ac780290fc92a2937454c1f5bc4d66997c4d5
buketuren <48716077+buketuren@users.noreply.github.com>
2019-04-24 14:17:56 +0300

    Create uygulama04.py

---
+++ uygulama04.py
@@ -0,0 +1,19 @@
+#Buket Üren
+#1.Soru
+metin = "Açık bilim, araştırma çıktılarına ve süreçlerine herkesin serbestçe erişmesini, bunların ortak kullanımını, dağıtımını ve üretimini kolaylaştıran bilim uygulamasıdır."
+print(metin[:19])
+
+#2.Soru
+liste = ["Açık Bilim", "Açık Erişim", "Açık Lisans", "Açık Eğitim", "Açık Veri", "Açık Kültür"]
+for kelime in liste:
+    print(kelime)
+
+#3.soru
+sozluk = {"Elma" : "Ağaçta yetişen bir tür meyve" , "Salatalık" : "Fidan üzerinde büyüyen bir tür sebze" }
+a = input("Bir meyve giriniz")
+a= a.capitalize()
+
+if a in sozluk:
+    print(sozluk[a])
+else:
+    print("Başka bir meyve giriniz.")
